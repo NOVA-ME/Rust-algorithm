3ef4327799ded3b42a060c12a0106bde55fe14f0
NOVA-ME <nova-me@whatsoo.org>
2020-04-20 23:26:50 +0800

    bubble sort

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+mod sort;
+use sort::bubble;
+
 fn main() {
-    println!("Hello, world!");
+    let mut num_ary = vec![9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 1, 2, 3];
+    bubble::bubble_sort(&mut num_ary);
 }
