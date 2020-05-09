37f6f3c9cc6a41c77dda44d4a10a60514831a463
NOVA-ME <nova-me@whatsoo.org>
2020-05-09 23:45:06 +0800

    The biggest unrepeated substring length C D D D D D D The biggest length substring C C

--- src/main.rs
+++ src/main.rs
@@ -8,4 +8,17 @@ fn main() {
     leetcode_simple::two_sum_map_solve(&num_ary.clone(), 12);
     leetcode_simple::reverse_num(999999999999999999);
     leetcode_simple::reverse_num(1234000);
+    leetcode_simple::longest_sub_str("pwwkew".into());
+    leetcode_simple::longest_sub_str("abcabcbb".into());
+    leetcode_simple::longest_sub_str("aaaaaaa".into());
+    leetcode_simple::longest_sub_str(" ".into());
+    leetcode_simple::longest_sub_str("ausd".into());
+    leetcode_simple::longest_sub_str("aab".into());
+    leetcode_simple::longest_sub_str("cdd".into());
+    leetcode_simple::longest_sub_str("abba".into());
+    leetcode_simple::longest_sub_str("dvdf".into());
+    leetcode_simple::longest_sub_str("abbbc".into());
+    leetcode_simple::longest_sub_str("tmmzuxt".into());
+    leetcode_simple::longest_sub_str("abbcsef".into());    leetcode_simple::longest_sub_str("tmmzuxt".into());
+    leetcode_simple::longest_sub_str_better("abbcsef".into());
 }
