2440e248944e3781daf3110af4c57914c131bb47
NOVA-ME <nova-me@whatsoo.org>
2020-05-23 23:01:50 +0800

    Choose sort

--- src/main.rs
+++ src/main.rs
@@ -12,41 +12,42 @@ fn main() {
     ];
     sort::bubble_sort(&mut num_ary.clone());
     sort::select_sort(&mut num_ary.clone());
-    leetcode_simple::two_sum(&num_ary.clone(), 12);
-    leetcode_simple::two_sum_map_solve(&num_ary.clone(), 12);
-    leetcode_simple::reverse_num(999999999999999999);
-    leetcode_simple::reverse_num(1234000);
-    let start = SystemTime::now();
-    let mut values = Vec::new();
-    for i in str_ary.iter() {
-        values.push(leetcode_middle::longest_sub_str(String::from(*i)));
-    }
-    let end = SystemTime::now();
-    match end.duration_since(start) {
-        Ok(spend) => println!(
-            "自己的算法 结果：{:?}，耗时：{} 微秒",
-            values,
-            spend.as_micros()
-        ),
-        Err(_) => panic!("时间错误"),
-    }
-    values.clear();
-    let start = SystemTime::now();
+    sort::insert_sort(&mut num_ary.clone());
+    // leetcode_simple::two_sum(&num_ary.clone(), 12);
+    // leetcode_simple::two_sum_map_solve(&num_ary.clone(), 12);
+    // leetcode_simple::reverse_num(999999999999999999);
+    // leetcode_simple::reverse_num(1234000);
+    // let start = SystemTime::now();
+    // let mut values = Vec::new();
+    // for i in str_ary.iter() {
+    //     values.push(leetcode_middle::longest_sub_str(String::from(*i)));
+    // }
+    // let end = SystemTime::now();
+    // match end.duration_since(start) {
+    //     Ok(spend) => println!(
+    //         "自己的算法 结果：{:?}，耗时：{} 微秒",
+    //         values,
+    //         spend.as_micros()
+    //     ),
+    //     Err(_) => panic!("时间错误"),
+    // }
+    // values.clear();
+    // let start = SystemTime::now();
 
-    for i in str_ary.iter() {
-        values.push(leetcode_middle::longest_sub_str_better(String::from(*i)));
-    }
-    let end = SystemTime::now();
-    match end.duration_since(start) {
-        Ok(spend) => println!(
-            "网友的算法 结果：{:?}，耗时：{} 微秒",
-            values,
-            spend.as_micros()
-        ),
-        Err(_) => panic!("时间错误 "),
-    }
-    leetcode_middle::three_num(three_num_test);
-    let nums = vec![-5,-4,-3,-2,-1,0,0,1,2,3,4,5];
-    let res = leetcode_middle::four_nums_sum(nums, 0);
-    println!("{:?}", res);
+    // for i in str_ary.iter() {
+    //     values.push(leetcode_middle::longest_sub_str_better(String::from(*i)));
+    // }
+    // let end = SystemTime::now();
+    // match end.duration_since(start) {
+    //     Ok(spend) => println!(
+    //         "网友的算法 结果：{:?}，耗时：{} 微秒",
+    //         values,
+    //         spend.as_micros()
+    //     ),
+    //     Err(_) => panic!("时间错误 "),
+    // }
+    // leetcode_middle::three_num(three_num_test);
+    // let nums = vec![-5, -4, -3, -2, -1, 0, 0, 1, 2, 3, 4, 5];
+    // let res = leetcode_middle::four_nums_sum(nums, 0);
+    // println!("{:?}", res);
 }
--- src/sort.rs
+++ src/sort.rs
@@ -23,3 +23,18 @@ pub fn select_sort(ary: &mut Vec<i32>) {
     }
     println!("经过选择排序后的{:?}", ary);
 }
+
+pub fn insert_sort(ary: &mut Vec<i32>) {
+    println!("未经过插入排序 {:?}", ary);
+    // 插入排序假定前面是一个有序的数组a，这里假定第一个数字是有序的数组a
+    for i in 1..ary.len() {
+        let temp = ary[i];
+        let mut j = i;
+        while j > 0 && temp < ary[j - 1] {
+            ary[j] = ary[j - 1];
+            j -= 1;
+        }
+        ary[j] = temp;
+    }
+    println!("经过插入排序后 {:?}", ary);
+}
